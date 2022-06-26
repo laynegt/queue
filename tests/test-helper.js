7921c73e7086749517fa28def0741ddb0b0e6000
Layne Tcheng <laynegt@gmail.com>
2022-06-26 17:47:03 -0400

    ember 4.4

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'queue/app';
+import config from 'queue/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
